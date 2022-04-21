0cd32caf0d190430961d6aaf5ca4506aadadcc0e
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-04-21 09:43:06 -0300

    feat: add /sign-up

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(json());
+server.use(cors())
 
 server.listen(5000, () => {
     console.log('Servidor iniciado em http://localhost:5000');
+});
+
+const users = [];
+const tweets = [];
+
+server.post('/sign-up', (req, res) => {
+    users.push(req.body);
+    res.send('OK');
 });
