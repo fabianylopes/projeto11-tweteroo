b61b5e59413217e512d0923b90ba0fd384d85ce1
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-04-23 07:36:06 -0300

    feat: add tweets avatar

--- index.js
+++ index.js
@@ -5,30 +5,37 @@ const server = express();
 server.use(json());
 server.use(cors());
 
-server.listen(5000, () => {
-    console.log('Running on http://localhost:5000');
-});
-
 const users = [];
 const tweets = [];
 
 server.post('/sign-up', (req, res) => {
-    users.push(req.body);
+    const body = req.body;
+    users.push(body);
+    
     res.send('OK');
 });
 
 server.post('/tweets', (req, res) => {
-    tweets.push(req.body);
+    const tweet = req.body;
+
+    const userInfo = users.find(user => user.username === tweet.username);
+    tweets.push({...tweet, avatar: userInfo.avatar});
+
     res.send('OK');
 });
 
 server.get('/tweets', (req, res) => {
-   const lastTweets = [];
+    const lastTweets = [];
 
-   for(let i = tweets.length - 1; i >= tweets.length - 10; i--){
+    for(let i = tweets.length - 1; i >= tweets.length - 10; i--){
         if(tweets[i]){
             lastTweets.push(tweets[i]);
         }    
-   }
+    }
+
     res.send(lastTweets);
+});
+
+server.listen(5000, () => {
+    console.log('Running on http://localhost:5000');
 });
